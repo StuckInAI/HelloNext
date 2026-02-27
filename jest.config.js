b46bcea687d65f8043625c7e45ae6ffbcc8a1d3c
Tasnim Ahmed <tasnim@tazzix.com>
2026-02-27 15:53:27 +0500

    feat: Create a Hello World kind fullstack app using Next...

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/coverage/**',
+  ],
+}
+
+module.exports = createJestConfig(config)
